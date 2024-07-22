50cd530ca48552db4471346d98c8e69d9a7f32ac
sinamong0620 <sinaskim60@gmail.com>
2024-07-22 19:34:38 +0900

    #1 feat: navbar css 완료

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,73 @@
-import React from 'react';
+import styled from 'styled-components';
+import theme from '../styles/Theme/Theme';
+import rightarrow from '../assets/images/rightarrow.png';
+import NavButton from './NavButton';
+import Dashboard from './Dashboard';
 
 const Navbar = () => {
-  return <></>;
+  return (
+    <NavBarLayout>
+      <UserInfoContainer>
+        <ProfileImageWrapper>
+          <img />
+        </ProfileImageWrapper>
+        <UserDetailContainer>
+          <p>홍길동님</p>
+          <a>
+            마이페이지 <img src={rightarrow} alt="마이페이지 이동 화살표" />
+          </a>
+        </UserDetailContainer>
+      </UserInfoContainer>
+      <ButtonContainer>
+        <NavButton variant="primary">대시보드 생성</NavButton>
+        <NavButton variant="secondary">도전! 챌린지</NavButton>
+      </ButtonContainer>
+      <DashboardsContainer>
+        <Dashboard text="개인" />
+        <Dashboard text="팀" />
+      </DashboardsContainer>
+    </NavBarLayout>
+  );
 };
 export default Navbar;
+
+const NavBarLayout = styled.nav`
+  width: 12.5rem;
+  background: ${theme.color.navbar};
+  padding: 2.5625rem 1.625rem;
+`;
+
+const UserInfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  margin-bottom: 2.5rem;
+`;
+
+const ProfileImageWrapper = styled.div`
+  width: 45px;
+  height: 45px;
+  background-color: ${theme.color.black};
+  border-radius: 50%;
+`;
+
+const UserDetailContainer = styled.div`
+  p {
+    font-size: ${theme.font.size.main};
+    font-weight: ${theme.font.weight.medium};
+    margin-bottom: 4px;
+  }
+
+  a {
+    font-size: ${theme.font.size.caption};
+    font-weight: ${theme.font.weight.light};
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1.875rem;
+`;
+
+const DashboardsContainer = styled.div``;
---
+++ src/styles/Theme/Theme.ts
@@ -0,0 +1,34 @@
+const theme = {
+  color: {
+    black: '#000000',
+    white: '#FFFFFF',
+    main: '#4C8CFF',
+    main2: '#9847FF',
+    main3: '#00D1FF',
+    text: '#2A2C30',
+    gray: '#858585',
+    lightGray: '#D1D1D1',
+    navbar: '#F5F5F5',
+    gradation: 'linear-gradient(90deg, #4C8CFF 0%, #9847FF 100%)',
+  },
+  borderRadius: {},
+  font: {
+    size: {
+      mainTitle: '20px',
+      main: '16px',
+      caption: '12px',
+    },
+    weight: {
+      light: 300,
+      regular: 400,
+      medium: 500,
+      semiBold: 600,
+      bold: 700,
+      extraBold: 800,
+    },
+  },
+};
+
+export type ThemeColor = keyof typeof theme.color;
+
+export default theme;

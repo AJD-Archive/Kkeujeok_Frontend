bbfecb1fb5f9ed57d53ba2d8f9a982a9bee40e6a
sinamong0620 <sinaskim60@gmail.com>
2024-07-28 02:41:43 +0900

    #1 feat: add global styles and reset CSS

---
+++ src/styles/GlobalStyle/GlobalStyle.tsx
@@ -0,0 +1,43 @@
+import { css, Global } from '@emotion/react';
+import { resetCss } from './ reset';
+
+export default function GlobalStyle() {
+  return <Global styles={globalCss} />;
+}
+
+const globalCss = () => css`
+  ${resetCss}
+
+  :root {
+    font-family: Pretendard;
+
+    body {
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+
+      .draggable {
+        -webkit-user-select: all;
+        -moz-user-select: all;
+        -ms-user-select: all;
+        user-select: all;
+      }
+    }
+
+    * {
+      box-sizing: border-box;
+      font-family: inherit;
+      word-break: keep-all;
+      word-wrap: break-word;
+
+      -webkit-tap-highlight-color: rgba(0, 0, 0, 0); /* iOS Button Active */
+
+      -ms-overflow-style: none; /* IE 11 */
+      scrollbar-width: none; /* Firefox 64 */
+      ::-webkit-scrollbar {
+        display: none !important;
+      }
+    }
+  }
+`;

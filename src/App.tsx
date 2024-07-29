01719cf851583e1adeb0c3e6c0104cf505917fad
MyungJiwoo <1206jiwoo@gmail.com>
2024-07-29 15:12:49 +0900

    #6 feature: 카카오, 구글 소셜 로그인

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
+import OAuthRedirectHandler from './contexts/OAuthRedirectHandler';
+import { AuthProvider } from './contexts/AuthContext';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" Component={MainPage} />
-        <Route path="/login" Component={LoginPage} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" Component={MainPage} />
+          <Route path="/login" Component={LoginPage} />
+          <Route path="/api/oauth2/callback/:provider" element={<OAuthRedirectHandler />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,12 @@ import { Reset } from 'styled-reset';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <Reset />
     <App />
-  </React.StrictMode>
+  </>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

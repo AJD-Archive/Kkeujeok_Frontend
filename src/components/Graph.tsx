2aa590d7692ebaf87bda193c3084fe5d7a30771f
sinamong0620 <sinaskim60@gmail.com>
2024-08-06 17:13:15 +0900

    #1 style : 스타일 코드 분리

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,40 +1,12 @@
-import { styled } from 'styled-components';
-import theme from '../styles/Theme/Theme';
+import * as S from '../styles/DashboardStyled';
 
 const Graph = () => {
   return (
-    <GraphContainer>
-      <GraphProgress>
+    <S.GraphContainer>
+      <S.GraphProgress>
         <p>60%</p>
-      </GraphProgress>
-    </GraphContainer>
+      </S.GraphProgress>
+    </S.GraphContainer>
   );
 };
 export default Graph;
-
-const GraphContainer = styled.div`
-  width: 13.1875rem;
-  height: 1.3125rem;
-
-  background: none;
-  border: 1px solid ${theme.color.lightGray};
-  border-radius: 30px;
-
-  p {
-    margin-left: 1rem;
-    font-size: ${theme.font.size.caption};
-    color: ${theme.color.white};
-  }
-`;
-
-const GraphProgress = styled.div`
-  width: 60%; /*todo의 완료도에 따라 부모 width의 퍼센테이지로 맞춰 크기 조정*/
-  height: 1.3125rem;
-
-  background: ${theme.color.gradation};
-  border: none;
-  border-radius: 30px;
-
-  display: flex;
-  align-items: center;
-`;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,87 +1,32 @@
-import styled from 'styled-components';
-import theme from '../styles/Theme/Theme';
 import rightarrow from '../assets/images/rightarrow.png';
-import NavButton from './NavButton';
+import * as S from '../styles/NavBarStyled';
 import Dashboard from './Dashboard';
 
 const Navbar = () => {
   return (
-    <NavBarLayout>
+    <S.NavBarLayout>
       <div>
-        <UserInfoContainer>
-          <ProfileImageWrapper>
+        <S.UserInfoContainer>
+          <S.ProfileImageWrapper>
             <img />
-          </ProfileImageWrapper>
-          <UserDetailContainer>
+          </S.ProfileImageWrapper>
+          <S.UserDetailContainer>
             <p>홍길동님</p>
             <a>
               마이페이지 <img src={rightarrow} alt="마이페이지 이동 화살표" />
             </a>
-          </UserDetailContainer>
-        </UserInfoContainer>
-        <ButtonContainer>
-          <NavButton variant="primary">대시보드 생성</NavButton>
-          <NavButton variant="secondary">도전! 챌린지</NavButton>
-        </ButtonContainer>
+          </S.UserDetailContainer>
+        </S.UserInfoContainer>
+        <S.ButtonContainer>
+          <S.NavButton variant="primary">대시보드 생성</S.NavButton>
+          <S.NavButton variant="secondary">도전! 챌린지</S.NavButton>
+        </S.ButtonContainer>
       </div>
-      <DashboardsContainer>
+      <S.DashboardsContainer>
         <Dashboard text="개인" />
         <Dashboard text="팀" />
-      </DashboardsContainer>
-    </NavBarLayout>
+      </S.DashboardsContainer>
+    </S.NavBarLayout>
   );
 };
 export default Navbar;
-
-const NavBarLayout = styled.nav`
-  width: 12.5rem;
-  min-width: 12.5rem;
-  display: flex;
-  flex-direction: column;
-  background: ${theme.color.navbar};
-  padding: 2.5625rem 1.625rem;
-`;
-
-const UserInfoContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-  margin-bottom: 2.5rem;
-`;
-
-const ProfileImageWrapper = styled.div`
-  width: 45px;
-  height: 45px;
-  background-color: ${theme.color.black};
-  border-radius: 50%;
-`;
-
-const UserDetailContainer = styled.div`
-  p {
-    font-size: ${theme.font.size.main};
-    font-weight: ${theme.font.weight.medium};
-    margin-bottom: 4px;
-  }
-
-  a {
-    display: flex;
-    align-items: center;
-    font-size: ${theme.font.size.caption};
-    font-weight: ${theme.font.weight.light};
-    color: ${theme.color.gray};
-  }
-
-  img {
-    width: 12px;
-  }
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 1.875rem;
-`;
-
-const DashboardsContainer = styled.div`
-  overflow-y: auto;
-`;

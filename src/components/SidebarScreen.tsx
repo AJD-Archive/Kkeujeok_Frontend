e7dafe0f099f05ee09afeb2b8e7a1a97e29ccf89
sinamong0620 <sinaskim60@gmail.com>
2024-08-19 21:17:21 +0900

    #11 feat: 사이드스크린 기능 추가

--- src/components/SidebarScreen.tsx
+++ src/components/SidebarScreen.tsx
@@ -2,19 +2,45 @@ import Profile from './Profile';
 import Flex from './Flex';
 import edit from '../img/edit.png';
 import trash from '../img/delete2.png';
+import { visibleAtom } from '../contexts/sideScreenAtom';
+import { useAtom } from 'jotai';
+import closebutton from '../img/closebutton.png';
+import { useRef, useState } from 'react';
 import * as S from '../styles/SideScreenStyled';
 
 const SidebarScreen = () => {
+  const [inputText, setInputText] = useState<string>('');
+  const [_, setVisibleValue] = useAtom(visibleAtom);
+
+  const editorRef = useRef<HTMLDivElement>(null);
+
+  //사이드 스크린 상태 변수 변경 함수
+  const toggleFunc = (event: React.MouseEvent) => {
+    setVisibleValue(prev => !prev);
+    event.stopPropagation();
+  };
+
+  const onInputHandler = (event: React.SyntheticEvent<HTMLDivElement>) => {
+    setInputText((event.target as HTMLDivElement).innerText);
+  };
+
   return (
-    <S.SideScreenContainer>
-      <S.SideScreen>
+    <S.SideScreenContainer onClick={toggleFunc}>
+      <S.SideScreen
+        onClick={e => {
+          e.stopPropagation();
+        }}
+      >
+        <S.ImgWrapper>
+          <img src={closebutton} alt="닫기 버튼" onMouseDown={toggleFunc} />
+        </S.ImgWrapper>
         <header>
           <Flex gap="0.6785rem" margin="0 0 0.5rem 0">
             <Profile width="35px" height="35px" />
             <span>김신아</span>
           </Flex>
           <S.TitleContainer>
-            <h1>기능 명세서</h1>
+            <input placeholder="제목을 입력해주세요" />
             <Flex gap="0.5625rem">
               <img src={edit} alt="편집 버튼" />
               <img src={trash} alt="휴지통 버튼" />
@@ -23,9 +49,11 @@ const SidebarScreen = () => {
           <hr></hr>
         </header>
 
-        <section>
-          <textarea></textarea>
-        </section>
+        <S.TextEditingWrapper
+          contentEditable
+          onInput={onInputHandler}
+          ref={editorRef}
+        ></S.TextEditingWrapper>
       </S.SideScreen>
     </S.SideScreenContainer>
   );
--- src/styles/SideScreenStyled.tsx
+++ src/styles/SideScreenStyled.tsx
@@ -1,4 +1,5 @@
 import { styled } from 'styled-components';
+import theme from './Theme/Theme';
 
 export const SideScreenContainer = styled.div`
   background: rgba(0, 0, 0, 0.1);
@@ -38,5 +39,48 @@ export const TitleContainer = styled.div`
   img {
     width: 20px;
     height: 20px;
+  }
+
+  input {
+    width: 100%;
+    margin-right: 1rem;
+
+    outline: none;
+    border: none;
+    font-size: 20px;
+    font-weight: 900;
+  }
+
+  input::placeholder {
+    color: ${theme.color.lightGray};
+  }
+`;
+
+export const ImgWrapper = styled.div`
+  width: 15px;
+  height: 15px;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+  }
+`;
+
+export const TextEditingWrapper = styled.div`
+  &:focus {
+    outline: none;
+  }
+
+  &:empty:not(:focus)::before {
+    content: '여기에 글을 입력해주세요';
+    color: ${theme.color.lightGray};
+  }
+
+  div {
+    margin: 0.625rem 0;
   }
 `;

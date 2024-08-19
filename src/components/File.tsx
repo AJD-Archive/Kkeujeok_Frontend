e136980f5dfc138380e4b7378b7b3c8a79e45c0f
sinamong0620 <sinaskim60@gmail.com>
2024-08-19 21:16:58 +0900

    #11 feat: 팀 문서 기능 추가

--- src/components/File.tsx
+++ src/components/File.tsx
@@ -1,15 +1,21 @@
 import fileimg from '../img/fileimg.png';
 import Flex from './Flex';
 import * as S from '../styles/TeamDocumentStyled';
-
+import { visibleAtom } from '../contexts/sideScreenAtom';
+import { useAtom } from 'jotai';
 type Props = {
   caption: string;
 };
 const File = ({ caption }: Props) => {
+  const [_, setVisibleValue] = useAtom(visibleAtom);
+
+  const toggleFunc = () => {
+    setVisibleValue(prev => !prev);
+  };
   return (
     <S.FileImgWrapper>
       <Flex flexDirection="column" justifyContent="center">
-        <img src={fileimg} alt="파일 이모티콘" />
+        <img src={fileimg} alt="파일 이모티콘" onClick={toggleFunc} />
         <span>{caption}</span>
       </Flex>
     </S.FileImgWrapper>
--- src/pages/TeamFileBoard.tsx
+++ src/pages/TeamFileBoard.tsx
@@ -5,21 +5,27 @@ import File from '../components/File';
 import SidebarScreen from '../components/SidebarScreen';
 import * as S from '../styles/MainPageStyled';
 import { FolderContainer } from '../styles/TeamDocumentStyled';
+import { visibleAtom } from '../contexts/sideScreenAtom';
+import { useAtom } from 'jotai';
 
-const TeamFileBoard = () => (
-  <S.MainDashBoardLayout>
-    <Navbar />
-    <S.MainDashBoardContainer>
-      <Header mainTitle="기획" subTitle="기획을 설명해주세요" />
-      <FolderContainer>
-        <Flex gap="4.5625rem">
-          <File caption="기능명세서" />
-          <File caption="ERD" />
-          <File caption="color" />
-        </Flex>
-      </FolderContainer>
-      <SidebarScreen />
-    </S.MainDashBoardContainer>
-  </S.MainDashBoardLayout>
-);
+const TeamFileBoard = () => {
+  const [visibleValue, _] = useAtom(visibleAtom);
+
+  return (
+    <S.MainDashBoardLayout>
+      <Navbar />
+      <S.MainDashBoardContainer>
+        <Header mainTitle="기획" subTitle="기획을 설명해주세요" />
+        <FolderContainer>
+          <Flex gap="4.5625rem">
+            <File caption="기능명세서" />
+            <File caption="ERD" />
+            <File caption="color" />
+          </Flex>
+        </FolderContainer>
+        {visibleValue && <SidebarScreen />}
+      </S.MainDashBoardContainer>
+    </S.MainDashBoardLayout>
+  );
+};
 export default TeamFileBoard;

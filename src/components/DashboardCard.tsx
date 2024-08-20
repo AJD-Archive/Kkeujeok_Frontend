297d324ecd4c36fa49f284b6e24c1500d28af1ed
MyungJiwoo <1206jiwoo@gmail.com>
2024-08-20 11:40:41 +0900

    #17 refactor: 블록의 '상태' 변수명을 api 파라미터와 통일

--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -4,16 +4,16 @@ import * as S from '../styles/DashboardStyled';
 type Props = {
   backGroundColor?: string;
   highlightColor?: string;
-  state?: string;
+  progress?: string;
   imgSrc?: string;
 };
 
-const DashboardCard = ({ backGroundColor, highlightColor, state, imgSrc }: Props) => {
+const DashboardCard = ({ backGroundColor, highlightColor, progress, imgSrc }: Props) => {
   return (
     <S.CardContainer backGroundColor={backGroundColor}>
       <header>
         <S.StatusBarContainer highlightColor={highlightColor}>
-          <span>{state}</span>
+          <span>{progress}</span>
         </S.StatusBarContainer>
         <S.AddButtonWrapper>
           <img src={imgSrc} alt="블록 더하는 버튼" />
@@ -26,3 +26,17 @@ const DashboardCard = ({ backGroundColor, highlightColor, state, imgSrc }: Props
   );
 };
 export default DashboardCard;
+
+/*
+* 블록 생성시 필요한 정보
+{
+  "dashboardId" : 1,
+  "title" : "Title",
+  "contents" : "Contents",
+  "progress" : "NOT_STARTED",
+  "deadLine" : "2024.08.03 13:23"
+}
+
+! 플러스 버튼을 눌렀을때 빈 title, contents, deadLine이 들어간 post 요청을 보내고,
+! 사이드 페이지에서 1분 마다 자동 저장 + 다른 페이지로 넘어가면 자동 저장은 patch 요청
+*/

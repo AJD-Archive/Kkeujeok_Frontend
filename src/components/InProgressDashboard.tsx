051fac5d924d671c4e53fa49f62863d240cdd730
MyungJiwoo <1206jiwoo@gmail.com>
2024-08-20 20:25:39 +0900

    #17 refactor: 상태별 대시보드로 분리 (시작 전, 진행 중, 완료)

---
+++ src/components/InProgressDashboard.tsx
@@ -0,0 +1,41 @@
+import { useNavigate, Outlet } from 'react-router-dom';
+import Block from './Block';
+import * as S from '../styles/DashboardStyled';
+import { useAtom } from 'jotai';
+import { visibleAtom } from '../contexts/sideScreenAtom';
+import SidePage from '../pages/SidePage';
+
+type Props = {
+  backGroundColor?: string;
+  highlightColor?: string;
+  progress?: string;
+  imgSrc?: string;
+};
+
+const InProgressDashboard = ({ backGroundColor, highlightColor, progress, imgSrc }: Props) => {
+  const navigate = useNavigate();
+  const [visibleValue, _] = useAtom(visibleAtom);
+
+  const handleAddBtn = () => {
+    console.log(progress);
+    navigate(`/side/1`, { state: { highlightColor, progress } });
+  };
+
+  return (
+    <S.CardContainer backGroundColor={backGroundColor}>
+      <header>
+        <S.StatusBarContainer highlightColor={highlightColor}>
+          <span>{progress}</span>
+        </S.StatusBarContainer>
+        <S.AddButtonWrapper onClick={handleAddBtn}>
+          <img src={imgSrc} alt="블록 더하는 버튼" />
+        </S.AddButtonWrapper>
+      </header>
+      <section>
+        <Block />
+      </section>
+      <Outlet />
+    </S.CardContainer>
+  );
+};
+export default InProgressDashboard;

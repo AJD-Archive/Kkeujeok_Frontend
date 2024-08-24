8b394a426c13da8bcf078830a88c430f0e56a20b
MyungJiwoo <1206jiwoo@gmail.com>
2024-08-24 17:38:46 +0900

    #19 feature: 사이드 페이지 자동 저장 기능 구현

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+// 제네릭으로 모든 타입을 받을 수 있는 debounce 훅
+export const useDebounce = <T>(value: T, delay: number): T => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};

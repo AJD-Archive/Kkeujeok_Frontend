8ca3e26a4116016746ceac634dd5538c47d34d46
sinamong0620 <sinaskim60@gmail.com>
2024-08-29 17:57:39 +0900

    #11 chore : merge commit

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,5 @@
 import Navbar from '../components/Navbar';
 import theme from '../styles/Theme/Theme';
-import deleteicon from '../img/delete2.png';
 import main from '../img/main.png';
 import main2 from '../img/main2.png';
 import main3 from '../img/main3.png';
@@ -8,7 +7,11 @@ import Header from '../components/Header';
 import * as S from '../styles/MainPageStyled';
 import NotStartedDashboard from '../components/NotStartedDashboard';
 import InProgressDashboard from '../components/InProgressDashboard';
-import DoneDashboard from '../components/DoneDashboard';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import { useState } from 'react';
+import { handleAutoScroll } from '../utils/handleAutoScroll';
+
+export type TItemStatus = 'todo' | 'doing' | 'done';
 
 const MainPage = () => {
   return (
@@ -16,30 +19,14 @@ const MainPage = () => {
       <Navbar />
       <S.MainDashBoardContainer>
         <Header mainTitle="개인 대시보드" subTitle="개인 대시보드 설명하는 자리입니다" />
-        <S.CardContainer>
-          <NotStartedDashboard
-            backGroundColor="#E8FBFF"
-            highlightColor={theme.color.main3}
-            progress="시작 전"
-            imgSrc={main3}
-          />
-          <InProgressDashboard
-            backGroundColor="#EDF3FF"
-            highlightColor={theme.color.main}
-            progress="진행 중"
-            imgSrc={main}
-          />
-          <DoneDashboard
-            backGroundColor="#F7F1FF"
-            highlightColor={theme.color.main2}
-            progress="완료"
-            imgSrc={main2}
-          />
-        </S.CardContainer>
-
-        <S.DeleteIcon>
-          <img src={deleteicon} alt="휴지통아이콘" />
-        </S.DeleteIcon>
+        <DragDropContext onDragEnd={onDragEnd} onDragUpdate={handleAutoScroll}>
+          <S.CardContainer>
+            {Object.values(columns).map(column => {
+              const { id, component: DashboardComponent, ...props } = column;
+              return <DashboardComponent key={id} id={id} {...props} />;
+            })}
+          </S.CardContainer>
+        </DragDropContext>
       </S.MainDashBoardContainer>
     </S.MainDashBoardLayout>
   );

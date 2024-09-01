282061c3557a2e00ea271ac27f0561acbe3579b7
MyungJiwoo <1206jiwoo@gmail.com>
2024-09-02 03:29:36 +0900

    #26 feature: 사이드 페이지 확인 및 자동 저장 api 연결

--- src/types/PersonalBlock.ts
+++ src/types/PersonalBlock.ts
@@ -1,7 +1,23 @@
-export interface PersonalBlock {
-  dashboardId: number;
-  title: string;
-  contents: string;
-  progress: string;
-  deadLine: string;
+export interface StatusPersonalBlock {
+  blockListResDto: BlockListResDto[];
+  pageInfoResDto: PageInfoResDto;
+}
+
+export interface BlockListResDto {
+  dashboardId?: string; // 지우) 따로 추가
+  blockId?: string | null;
+  title?: string;
+  contents?: string;
+  progress?: string;
+  type?: string;
+  startDate?: string | null;
+  deadLine?: string | null;
+  nickname?: string;
+  dDay?: number;
+}
+
+export interface PageInfoResDto {
+  currentPage: number;
+  totalPages: number;
+  totalItems: number;
 }

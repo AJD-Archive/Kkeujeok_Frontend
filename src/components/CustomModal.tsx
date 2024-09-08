b9843bbdb8054bef02dc8be20cf2bf61dec82c74
MyungJiwoo <1206jiwoo@gmail.com>
2024-09-08 20:51:29 +0900

    #45 refactor: 모달창에 함수를 전달하여 사용

--- src/components/CustomModal.tsx
+++ src/components/CustomModal.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import ErrorIcon from '../img/error.png';
 import Flex from './Flex';
-import {} from '../styles/CreateBoardPageStyled';
 import {
   StyledModal,
   customStyles,
@@ -14,30 +13,22 @@ import {
 } from '../styles/ModalStyled';
 import { CustomModalProps } from '../types/CustomModal';
 
-/*
->>>>> 모달창 사용 방법
-<CustomModal title="블록을 삭제하시겠습니까?" subTitle="삭제 이후 되돌릴 수 없습니다." />
-이렇게 컴포넌트로 불러와서 사용할 수 있습니다.
-
-추후 기능에 맞춰 다른 함수를 받아 사용할 수 있도록 수정할 예정입니다.
-*/
-
-const CustomModal = ({ title, subTitle, onClose }: CustomModalProps) => {
+const CustomModal = ({ title, subTitle, onYesClick, onNoClick }: CustomModalProps) => {
   return (
     <StyledModal
       isOpen={true}
       shouldFocusAfterRender={false}
-      onRequestClose={onClose}
+      onRequestClose={onNoClick} // 기본적으로 No 버튼 클릭 시 모달 닫힘
       style={customStyles}
     >
-      <ErrorImg src={ErrorIcon} alt="error_icon"></ErrorImg>
+      <ErrorImg src={ErrorIcon} alt="error_icon" />
       <Flex flexDirection="column">
         <SubTitle>{subTitle}</SubTitle>
         <Title>{title}</Title>
       </Flex>
       <Flex>
-        <BtnYes onClick={onClose}>예</BtnYes>
-        <BtnNo onClick={onClose}>아니오</BtnNo>
+        <BtnYes onClick={onYesClick}>예</BtnYes>
+        <BtnNo onClick={onNoClick}>아니오</BtnNo>
       </Flex>
     </StyledModal>
   );
--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,12 +1,86 @@
+/*
+* 모달창 사용법
+1) 비즈니스 로직
+openModal()에 원하는 버튼과 원하는 함수를 최대 2쌍 (즉 총 인수 4개) 넣을 수 있음
+예를 들어, yes 버튼이 눌렸을때만 작동할 함수는 openModal('yes', 함수이름)
+그리고 각각 다른 함수를 하고 싶다면 openModal('yes', 함수이름1, 'no', 함수이름2)
+만약 특별한 함수 없이 단순 정보를 보여주기 위한 모달이라면 openModal(normal)로 어떤 버튼을 누르든 모달창이 꺼지도록 호출 가능
+
+2) 모달창 컴포넌트 (모달창 조건과 title, subTitle만 변경. 나머지 고정)
+      {isModalOpen && isEmptyModalOpen && (
+        <CustomModal
+          title="모든 칸을 작성해주세요."
+          subTitle="잠깐! 작성되지 않은 칸이 있습니다."
+          onYesClick={handleYesClick}
+          onNoClick={handleNoClick}
+        />
+      )}
+
+3) 모달창 조건 관리 : 비즈니스 로직에서
+ setIsEmptyModalOpen(true);
+const handleModalClose = () => setIsEmptyModalOpen(false);
+openModal('normal', handleModalClose); 
+2번과 같은 모달창을 여러 조건에 따라 띄워야 한다면 조건을 on/off할 수 있도록 상태 함수를 콜백으로 전달 받는다.
+그리고 openModal()함수가 종료됨에 따라 해당 조건을 다시 off로 바꿔, 모달창 여러 개가 취소되더라도 섞이지 않도록 한다.
+*/
 import { useState } from 'react';
+
+type ModalAction = 'yes' | 'no' | 'normal';
 
 export const useModal = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [actionHandlers, setActionHandlers] = useState<{
+    [key in ModalAction]: (() => void) | null;
+  }>({
+    yes: null,
+    no: null,
+    normal: null,
+  });
 
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+  // 콜백 함수 추가
+  const [onCloseCallback, setOnCloseCallback] = useState<(() => void) | null>(null);
 
-  return { isModalOpen, openModal, closeModal };
+  const openModal = (
+    action: ModalAction,
+    handler?: () => void,
+    noHandler?: () => void,
+    closeCallback?: () => void
+  ) => {
+    if (action === 'yes') {
+      setActionHandlers({ yes: handler || null, no: noHandler || null, normal: noHandler || null });
+    } else if (action === 'no') {
+      setActionHandlers({ yes: noHandler || null, no: handler || null, normal: noHandler || null });
+    } else if (action === 'normal') {
+      setActionHandlers({
+        yes: noHandler || null,
+        no: noHandler || null,
+        normal: noHandler || null,
+      });
+    }
+    setOnCloseCallback(closeCallback || null); // 콜백 함수 설정
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    if (onCloseCallback) {
+      onCloseCallback(); // 모달이 닫힐 때 콜백 호출 (여러 모달창이 있을 때 조건 관리를 위한 콜백 함수)
+    }
+  };
+
+  const handleYesClick = async () => {
+    if (actionHandlers.yes) {
+      await actionHandlers.yes(); // 사용자 정의 yes 클릭 핸들러 실행
+    }
+    closeModal(); // 모달을 닫음
+  };
+
+  const handleNoClick = () => {
+    if (actionHandlers.no) actionHandlers.no(); // 사용자 정의 no 클릭 핸들러 실행
+    closeModal(); // 모달을 닫음
+  };
+
+  return { isModalOpen, openModal, handleYesClick, handleNoClick };
 };
 
 export default useModal;

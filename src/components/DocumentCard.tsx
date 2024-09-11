29bbad8b9389f3cbd5020694123d74f4a6ea7fd0
MyungJiwoo <1206jiwoo@gmail.com>
2024-09-12 03:36:57 +0900

    #49 feat: 팀 문서 리스트 불러오기 &  카테고리별 검색 api 연결

--- src/components/DocumentCard.tsx
+++ src/components/DocumentCard.tsx
@@ -3,24 +3,27 @@ import Flex from '../components/Flex';
 import * as S from '../styles/TeamDocumentStyled';
 
 type Props = {
-  documentId: string;
-  title?: string;
-  category?: string;
-  name?: string;
-  profileImg?: string;
-  content?: string;
+  // documentId: string | undefined;
+  // title?: string;
+  // category?: string;
+  // name?: string;
+  // profileImg?: string;
+  // content?: string;
+  document: TeamDocument;
 };
 
-const DocumentCard = ({ documentId }: Props) => {
+const DocumentCard = ({ document }: Props) => {
   return (
     <S.DocumentComponent>
-      <Link to={`${documentId}`}>
+      <Link to={`${document.teamDocumentId}`}>
         <S.DocumentCard>
-          <S.DocumentCategory>카테고리</S.DocumentCategory>
-          <S.DocumentTitle>제목</S.DocumentTitle>
+          <S.DocumentCategory>{document.category}</S.DocumentCategory>
+          <S.DocumentTitle>{document.title}</S.DocumentTitle>
           <Flex>
-            <S.DocumentWriterImg></S.DocumentWriterImg>
-            <S.DocumnetWriter>작성자</S.DocumnetWriter>
+            <S.DocumentWriterImg>
+              <img src={document.picture} alt="프로필 사진" />
+            </S.DocumentWriterImg>
+            <S.DocumnetWriter>{document.author}</S.DocumnetWriter>
           </Flex>
         </S.DocumentCard>
       </Link>
--- src/pages/TeamDocumentBoard.tsx
+++ src/pages/TeamDocumentBoard.tsx
@@ -4,12 +4,17 @@ import Flex from '../components/Flex';
 import * as S from '../styles/TeamDocumentStyled';
 import { useLocation, useNavigate } from 'react-router-dom';
 import folderimg from '../img/folderimg.png';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import addbutton from '../img/addbutton.png';
 import leftarrow from '../img/leftarrow.png';
 import Pagination from '../components/CustomPagination';
 import DocumentCard from '../components/DocumentCard';
 import { Outlet } from 'react-router-dom';
+import {
+  createTeamDocument,
+  getTeamDocument,
+  getTeamDocumentCategories,
+} from '../api/TeamDocumentApi';
 
 const TeamDocumentBoard = () => {
   const location = useLocation();
@@ -18,24 +23,72 @@ const TeamDocumentBoard = () => {
   const [folderArray, setFolderArray] = useState<string[]>(['프론트엔드', '백엔드', '기획']);
   const [visible, setVisible] = useState<boolean>(false);
   const navigate = useNavigate();
-  const [count, setCount] = useState<number>(1); // 총 페이지 수
-  const [page, setPage] = useState<number>(1); // 현재 페이지
 
-  const handleNavigate = () => {
-    navigate('1'); // 상대 경로로 이동 `1`은 추후에 서버에서 받아오는 id값으로 대체할 예정임
-  };
+  const [page, setPage] = useState<number>(1); // 현재 페이지 (프론트에서 띄울 페이지. 1부터 시작)
+  const [size, setSize] = useState<number>(1); // 한 페이지에 요청할 요소 개수
 
-  const onChangeHandler = (e: React.FormEvent<HTMLInputElement>) => {
-    setFolder(e.currentTarget.value);
-  };
-  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setFolderArray([...folderArray, folder]);
-  };
+  const [categories, setCategories] = useState<string[]>([]);
+  const [teamDocuments, setTeamDocuments] = useState<TeamDocument[]>([]);
+  const [pageInfo, setPageInfo] = useState<PageInfoResDto>({
+    currentPage: 0,
+    totalPages: 1,
+    totalItems: 0,
+  }); // 백엔드에서 전달받은 페이지 dto. 0부터 시작. 따라서 page state를 따로 만들어 관리.
 
-  // 페이지네이션 페이지 변경 감지 함수
+  const [selectedCategory, setSelectedCategory] = useState<string>(''); // 선택된 카테고리로 재검색. 초기는 '' == 전체.
+
+  // * 페이지네이션 페이지 변경 감지 함수
   const handleChangePage = (event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value); // 페이지 변경 시 현재 페이지 상태 업데이트
+    setPageInfo(prevPageInfo => ({
+      ...prevPageInfo, // 기존 pageInfo 값을 유지
+      currentPage: value - 1,
+    }));
+  };
+
+  // * 팀 문서 카테고리 리스트 get
+  const fetchCategoriesData = async () => {
+    const response = await getTeamDocumentCategories(dashboardId);
+
+    if (response) setCategories(response);
+  };
+
+  // * 팀 문서 카테고리별 검색 get
+  const fetchDocumentData = async () => {
+    const response = await getTeamDocument(dashboardId, selectedCategory, pageInfo?.currentPage, 8);
+
+    if (response) {
+      setTeamDocuments(response.teamDocuments);
+      setPageInfo(response.pageInfoResDto);
+    }
+  };
+
+  useEffect(() => {
+    fetchDocumentData();
+    fetchCategoriesData();
+  }, [pageInfo.currentPage, location.pathname, selectedCategory]); // 페이지가 변경될 때와, 데이터가 변경되었을 때 (즉 라우터가 변경되었을 때) 리렌더링
+
+  // * 팀 문서 생성 post
+  const handleCreateTeamDocument = async () => {
+    const newDocument: TeamDocument = {
+      title: '',
+      content: '',
+      category: '',
+      teamDashboardId: dashboardId,
+    };
+
+    const documentId = await createTeamDocument(newDocument);
+    navigate(`${documentId}`); // 팀 문서 사이드 페이지로 이동
+  };
+
+  // * 팀 문서 카테고리로 선택하여 검색
+  const handleCategoryClick = async (category: string) => {
+    setSelectedCategory(category);
+    setPage(1);
+    setPageInfo(prevPageInfo => ({
+      ...prevPageInfo, // 기존 pageInfo 값을 유지
+      currentPage: 0,
+    }));
   };
 
   return (
@@ -44,44 +97,38 @@ const TeamDocumentBoard = () => {
       <S.MainDashBoardContainer>
         <S.Header>
           <Flex>
-            <img src={leftarrow} />
+            <img src={leftarrow} onClick={() => navigate(-1)} /> {/* 뒤로가기 버튼 */}
             <S.Title>팀 문서</S.Title>
           </Flex>
-          <img src={addbutton} />
+          <img src={addbutton} onClick={handleCreateTeamDocument} />
         </S.Header>
 
         <S.CategoriesContainer>
-          <S.Category>카테고리 1</S.Category>
-          <S.Category>카테고리 2</S.Category>
-          <S.Category>카테고리 3</S.Category>
-          <S.Category>카테고리 1</S.Category>
-          <S.Category>카테고리 2</S.Category>
-          <S.Category>카테고리 3</S.Category>
-          <S.Category>카테고리 1</S.Category>
-          <S.Category>카테고리 2</S.Category>
-          <S.Category>카테고리 3</S.Category>
-          <S.Category>카테고리 1</S.Category>
-          <S.Category>카테고리 2</S.Category>
-          <S.Category>카테고리 3</S.Category>
-          <S.Category>카테고리 1</S.Category>
-          <S.Category>카테고리 2</S.Category>
-          <S.Category>카테고리 3</S.Category>
+          {/* <S.Category>전체</S.Category> */}
+          {categories.map((category, index) => (
+            <S.Category
+              key={index}
+              onClick={() => handleCategoryClick(category)}
+              isSelected={selectedCategory === category}
+            >
+              {category}
+            </S.Category>
+          ))}
         </S.CategoriesContainer>
 
+        {/* 팀 문서 컴포넌트 */}
         <S.DocumentContainer>
-          <DocumentCard documentId="1" />
-          <DocumentCard documentId="1" />
-          <DocumentCard documentId="1" />
-          <DocumentCard documentId="1" />
-          <DocumentCard documentId="1" />
-          <DocumentCard documentId="1" />
+          {teamDocuments.map((document, index) => (
+            <DocumentCard key={index} document={document} />
+          ))}
         </S.DocumentContainer>
 
+        {/* 페이지네이션 */}
         <S.PaginationWrapper>
-          <Pagination count={count} page={page} onChange={handleChangePage} />
+          <Pagination count={pageInfo?.totalPages} page={page} onChange={handleChangePage} />
         </S.PaginationWrapper>
       </S.MainDashBoardContainer>
-      <Outlet /> {/* 중첩 라우팅 */}
+      <Outlet /> {/* 사이드 페이지를 위한 중첩 라우팅 */}
     </S.MainDashBoardLayout>
   );
 };

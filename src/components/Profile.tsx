8f5c0942d45e38e1ab7f9954f3ebce143e5bff29
MyungJiwoo <1206jiwoo@gmail.com>
2024-09-25 04:13:02 +0900

    #84 refactor: Navbar의 프로필 사진 로딩 중 오류시 대체 이미지 추가 & 이미지 최소 크기 보장

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,5 @@
 import * as S from '../styles/ProfileStyled';
+import defaultImage from '../img/default.png';
 
 export type Props = {
   width?: string;
@@ -6,9 +7,13 @@ export type Props = {
   profile?: string;
 };
 const Profile = ({ width, height, profile }: Props) => {
+  const handleImageError = (event: React.SyntheticEvent<HTMLImageElement>) => {
+    event.currentTarget.src = defaultImage; // 대체 이미지로 변경
+  };
+
   return (
     <S.ProfileImgWrapper width={width} height={height}>
-      <img src={profile} alt="프로필 이미지" />
+      <img src={profile} alt="프로필 이미지" onError={handleImageError} />
     </S.ProfileImgWrapper>
   );
 };

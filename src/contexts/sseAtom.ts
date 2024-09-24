ed7adf9172e0bb5bb8afb9952dc1fded61d579e6
sinamong0620 <sinaskim60@gmail.com>
2024-09-24 17:52:55 +0900

    #68 fix : 실시간 알람 렌더링 수정

--- src/contexts/sseAtom.ts
+++ src/contexts/sseAtom.ts
@@ -1,8 +1,8 @@
 import { atom } from 'jotai';
-import { EventSourcePolyfill } from 'event-source-polyfill';
+import { NotificationResponse } from '../types/MyPage';
 
 // Jotai atom 정의
 export const sseConnectedAtom = atom(false); // SSE 연결 상태
 export const sseMessagesAtom = atom<string[]>([]); // 수신된 알림 메시지
 export const unreadCount = atom<number>(0); // 읽지 않은 알림 수
-export const notifications = atom<string[]>([]); // 알림 목록 배열
+export const notifications = atom<NotificationResponse>(); // 알림 목록

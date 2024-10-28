7ef5a983627e2bbd613b9cc561f633b9ddb605db
MyungJiwoo <1206jiwoo@gmail.com>
2024-10-29 01:02:47 +0900

    #102 refactor: 안쓰는 코드 정리

--- src/utils/columnsConfig.ts
+++ src/utils/columnsConfig.ts
@@ -10,28 +10,28 @@ export type TItems = {
 };
 
 //폐기할 코드
-export const initialColumns = {
-  todo: {
-    id: 'todo',
-    list: [],
-    pageInfo: { currentPage: 0, totalPages: 1, totalItems: 1 },
-    component: NotStartedDashboard,
-  },
-  doing: {
-    id: 'doing',
-    list: [],
-    pageInfo: { currentPage: 0, totalPages: 1, totalItems: 1 },
-    component: InProgressDashboard,
-  },
-  done: {
-    id: 'done',
-    list: [],
-    pageInfo: { currentPage: 0, totalPages: 1, totalItems: 1 },
-    component: CompletedDashboard,
-  },
-  delete: {
-    id: 'delete',
-    list: [],
-    pageInfo: { currentPage: 0, totalPages: 1, totalItems: 0 },
-  },
-};
+// export const initialColumns = {
+//   todo: {
+//     id: 'todo',
+//     list: [],
+//     pageInfo: { currentPage: 0, totalPages: 1, totalItems: 1 },
+//     component: NotStartedDashboard,
+//   },
+//   doing: {
+//     id: 'doing',
+//     list: [],
+//     pageInfo: { currentPage: 0, totalPages: 1, totalItems: 1 },
+//     component: InProgressDashboard,
+//   },
+//   done: {
+//     id: 'done',
+//     list: [],
+//     pageInfo: { currentPage: 0, totalPages: 1, totalItems: 1 },
+//     component: CompletedDashboard,
+//   },
+//   delete: {
+//     id: 'delete',
+//     list: [],
+//     pageInfo: { currentPage: 0, totalPages: 1, totalItems: 0 },
+//   },
+// };

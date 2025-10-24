4446511c69b8287b5a13e0c1cc8ed62e43eb0ca9
HarrySeop <harryseop@gmail.com>
2025-10-24 15:36:56 +0900

    #139 chore: vite.config 수정 및 visualizer 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig, type PluginOption } from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react()],
+    plugins: [react(), visualizer() as PluginOption],
     resolve: {
-      // todo: 경로 별칭 추가하기
-      alias: [],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
     server: {
       port: 5173,

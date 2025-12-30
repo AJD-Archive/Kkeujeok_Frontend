605f8cd188ca1372fd0925d750f702b9d7fe2234
HarrySeop <harryseop@gmail.com>
2025-12-30 18:06:32 +0900

    #144 refactor: User 스키마 수정

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -2,24 +2,31 @@ import { z } from 'zod';
 
 import { socialTypeSchema } from '@/schemas/enums';
 
-/** 사용자 프로필 스키마 */
+/**
+ * 사용자 프로필 스키마
+ * 백엔드 MyPageInfoResDto 기준 - nullable 필드 수정됨
+ * 주의: nickName (camelCase N)
+ */
 export const userProfileSchema = z.object({
   memberId: z.number(),
   email: z.string().email(),
   name: z.string(),
-  nickName: z.string(),
-  picture: z.string().url(),
-  introduction: z.string(),
+  nickName: z.string().nullable(),
+  picture: z.string().nullable(),
+  introduction: z.string().nullable(),
   socialType: socialTypeSchema,
-  tag: z.string(),
+  tag: z.string().nullable(),
 });
 
-/** 팔로우 정보 스키마 */
+/**
+ * 팔로우 정보 스키마
+ * 백엔드 FollowInfoResDto 기준
+ */
 export const followInfoSchema = z.object({
   memberId: z.number(),
-  nickname: z.string(),
+  nickname: z.string().nullable(),
   name: z.string(),
-  profileImage: z.string().url(),
+  profileImage: z.string().nullable(),
   isFollow: z.boolean(),
 });
 

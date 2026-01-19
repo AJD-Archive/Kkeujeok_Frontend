8ea47a1d8201ea905d2f72f8a986ee9f9292781e
MyungJiwoo <1206jiwoo@gmail.com>
2026-01-19 15:12:42 +0900

    #145 feat: Block UI 컴포넌트 구현

---
+++ src/components/Block_new/Block.style.ts
@@ -0,0 +1,43 @@
+import { css } from '@emotion/react';
+
+export const containerStyle = css({
+  padding: '0 1rem',
+  border: '1px solid #eee',
+  backgroundColor: '#fff',
+  borderRadius: '0.75rem',
+  maxWidth: '20rem',
+});
+
+export const headerStyle = css({
+  display: 'flex',
+  justifyContent: 'space-between',
+  padding: '0.625rem 0',
+  borderBottom: '1px dashed #eee',
+});
+
+export const contentStyle = css({
+  padding: '0.75rem 0',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.375rem',
+});
+
+export const titleStyle = css({
+  fontSize: '1rem',
+  fontWeight: 'bold',
+  color: '#2A2C30',
+  textOverflow: 'ellipsis',
+  overflow: 'hidden',
+  whiteSpace: 'nowrap',
+});
+
+export const descriptionStyle = css({
+  fontSize: '0.875rem',
+  color: '#858585',
+  textOverflow: 'ellipsis',
+  display: '-webkit-box',
+  WebkitBoxOrient: 'vertical',
+  WebkitLineClamp: 2,
+  overflow: 'hidden',
+  lineHeight: '1.125rem',
+});
